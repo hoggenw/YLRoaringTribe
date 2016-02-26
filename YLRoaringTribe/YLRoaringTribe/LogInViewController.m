73a004e5a8c0f27302ca46137f7bc51620099239
千锋 <qianfeng@qianfengdeMac-mini.local>
2016-02-26 10:32:48 +0800

    参考文档地址

--- YLRoaringTribe/YLRoaringTribe/LogInViewController.m
+++ YLRoaringTribe/YLRoaringTribe/LogInViewController.m
@@ -59,6 +59,7 @@
     UITextField *text=(id)[self.view viewWithTag:i];
         if ([text isFirstResponder]) {
             [text resignFirstResponder];
+            [self AnimationDown];
             break;
         }
     }
@@ -69,16 +70,44 @@
 {
     if ([textField isFirstResponder]) {
     [textField resignFirstResponder];  // 取消第一响应者
+        [self AnimationDown];
       }
     return YES;
 }
 //当文本框变为第一响应者时候使用
 - (void)textFieldDidBeginEditing:(UITextField *)textField{
     if ([textField isFirstResponder]) {
-        for (UIView *temp in self.view.subviews) {
-        temp.frame=CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y - 100, self.view.frame.size.width, self.view.frame.size.height);
-        }
+        [self AnimationUp];
     }
     
 }
+//动画上移封装接口wlg
+-(void)AnimationUp{
+    //设置动画的名字
+    [UIView beginAnimations:@"Animation" context:nil];
+    //设置动画的间隔时间
+    [UIView setAnimationDuration:1.20];
+    //使用当前正在运行的状态开始下一段动画
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    for (UIView *temp in self.view.subviews) {
+        temp.frame=CGRectMake(temp.frame.origin.x, temp.frame.origin.y - 100, temp.frame.size.width, temp.frame.size.height);
+    }
+    //设置动画结束
+    [UIView commitAnimations];
+}
+//动画下移封装接口
+-(void)AnimationDown{
+    //设置动画的名字
+    [UIView beginAnimations:@"Animation" context:nil];
+    //设置动画的间隔时间
+    [UIView setAnimationDuration:0.20];
+    //使用当前正在运行的状态开始下一段动画
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    for (UIView *temp in self.view.subviews) {
+       temp.frame=CGRectMake(temp.frame.origin.x, temp.frame.origin.y +100, temp.frame.size.width, temp.frame.size.height);
+    }
+    //设置动画结束
+    [UIView commitAnimations];
+}
+
 @end
